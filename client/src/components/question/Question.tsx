f0008ebe7141065d8504dc6211e23398e1a35f8f
Ryan Thomas <ryanthomas30@gmail.com>
2022-04-20 19:55:26 -0400

    (client) Abstract out question

---
+++ client/src/components/question/Question.tsx
@@ -0,0 +1,182 @@
+
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import * as Yup from 'yup'
+
+import { Flexbox, Form, FormButton, Row, StringText, Text, TextInput } from '@/components'
+import { useAnswerQuestionMutation, GameQuery } from '@/apollo'
+import { theme } from '@/constants'
+import { getRandomColor, getWrongAnswerPhrases } from '@/util'
+
+interface QuestionProps {
+	questionNumber: number
+	question: GameQuery['game']['team']['questions'][number]
+	teamId?: string
+}
+
+interface FormValues {
+	answer: string
+}
+
+const InitialFormValues: FormValues = {
+	answer: '',
+}
+
+const formSchema = Yup.object().shape({
+	answer: Yup.string().required('Required'),
+})
+
+export const Question: React.FC<QuestionProps> = ({ questionNumber, question, teamId }) => {
+	const [wrongAnswerMessage, setWrongAnswerMessage] = useState<string>('')
+	const [answerQuestion, { loading: answerQuestionLoading }] = useAnswerQuestionMutation()
+
+	useEffect(() => {
+		setWrongAnswerMessage('')
+
+	}, [questionNumber])
+
+	const onFormSubmit = async ({ answer }: FormValues) => {
+		try {
+			if (!teamId) return
+			const { data } = await answerQuestion({
+				variables: {
+					answer: answer.trim(),
+					questionId: question.questionId,
+					teamId: teamId,
+				},
+			})
+			if (data?.answerQuestion.isCorrect === false) {
+				setWrongAnswerMessage(getWrongAnswerPhrases())
+			} else {
+				setWrongAnswerMessage('')
+			}
+		} catch (e) {
+			console.warn(e)
+		}
+	}
+
+	return (
+		<Flexbox
+			center
+			full
+			marginBetween='large'
+		>
+			<QuestionHeader
+				center
+			>
+				<Row center>
+					<Text
+						size={18}
+						color={getRandomColor()}
+					>
+						Question
+					</Text>
+					&nbsp;
+					<Text
+						size={18}
+						color={getRandomColor()}
+					>
+						{questionNumber + 1}
+					</Text>
+
+				</Row>
+			</QuestionHeader>
+			<ContentContainer
+				center
+				full
+				marginBetween='medium'
+			>
+				<Flexbox
+					align='center'
+					marginBetween='small'
+				>
+					<Text
+						fontWeight='bold'
+						color={theme.color.yellow}
+						size={32}
+					>
+						{question.title}
+					</Text>
+					<StringText size={14}>{question.description}</StringText>
+				</Flexbox>
+				{question.image?.url && <QuestionImage src={question.image.url} />}
+			</ContentContainer>
+			<FormContainer
+				full
+				center
+				maxWidth={400}
+				marginBetween='medium'
+			>
+				{!question.isCorrect && (
+					<Form
+						initialValues={InitialFormValues}
+						validationSchema={formSchema}
+						validateOnBlur
+						onSubmit={onFormSubmit}
+					>
+						<Flexbox
+							marginBetween='medium'
+							paddingBottom='medium'
+						>
+							<TextInput
+								label='Answer'
+								name='answer'
+								placeholder='Submit your answer'
+							/>
+						</Flexbox>
+						<FormButton
+							primary
+							disabled={answerQuestionLoading}
+						>
+							Submit
+						</FormButton>
+					</Form>
+				)}
+				{question.isCorrect && (
+					<CorrectAnswerBanner
+						center
+					>
+						Correct!
+					</CorrectAnswerBanner>
+				)}
+				{wrongAnswerMessage && (
+					<WrongAnswerBanner
+						center
+					>
+						{wrongAnswerMessage}
+					</WrongAnswerBanner>
+				)}
+			</FormContainer>
+		</Flexbox>
+	)
+}
+
+const QuestionImage = styled.img`
+	object-fit: cover;
+	object-position: center;
+	max-width: 100%;
+	max-height: 350px;
+	width: auto;
+	height: auto;
+`
+
+const QuestionHeader = styled(Row)`
+`
+
+const ContentContainer = styled(Flexbox)`
+`
+
+const FormContainer = styled(Flexbox)`
+`
+
+const CorrectAnswerBanner = styled(Flexbox)`
+	border-radius: ${theme.borderRadius};
+	background-color: ${theme.color.darkGreen};
+	color: ${theme.color.white};
+	height: 44px;
+	width: 100%;
+`
+
+const WrongAnswerBanner = styled(CorrectAnswerBanner)`
+	background-color: ${theme.color.red};
+`
--- client/src/pages/Questions.tsx
+++ client/src/pages/Questions.tsx
@@ -1,35 +1,28 @@
 
-import { useMemo, useState, useEffect } from 'react'
+import { useMemo } from 'react'
 import { Navigate, useNavigate, useParams } from 'react-router-dom'
-import { Button, Flexbox, Form, FormButton, Row, StringText, Text, TextInput, ProgressTracker, QuestionStatus } from '@/components'
-import { useGameQuery, useAnswerQuestionMutation } from '@/apollo'
-import { routeNames } from '@/routing'
 import styled from 'styled-components'
-import * as Yup from 'yup'
-import { theme } from '@/constants'
-import { getRandomColor, getWrongAnswerPhrases } from '@/util'
+
+import {
+	Button,
+	Flexbox,
+	Row,
+	Text,
+	ProgressTracker,
+	QuestionStatus,
+	Question,
+} from '@/components'
+import { useGameQuery } from '@/apollo'
+import { routeNames } from '@/routing'
 
 type QuestionsRouteParams = {
 	gameId: string
 	questionNumber: string
 }
 
-interface FormValues {
-	answer: string
-}
-
-const InitialFormValues: FormValues = {
-	answer: '',
-}
-
-const formSchema = Yup.object().shape({
-	answer: Yup.string().required('Required'),
-})
-
 export const Questions: React.FC = () => {
 	const { gameId, questionNumber } = useParams<QuestionsRouteParams>()
 	const navigate = useNavigate()
-	const [wrongAnswerMessage, setWrongAnswerMessage] = useState<string>('')
 	const { data } = useGameQuery({
 		variables: {
 			gameId: gameId!,
@@ -37,17 +30,11 @@ export const Questions: React.FC = () => {
 		fetchPolicy: 'cache-only',
 		skip: !gameId,
 	})
-	const [answerQuestion, { loading: answerQuestionLoading }] = useAnswerQuestionMutation()
 
 	const questions = useMemo(() => data?.game.team.questions ?? [], [data])
 	const team = data?.game.team
 	const otherTeams = data?.game.teams.filter((t) => t.id !== team?.id) ?? []
 	const numberOfQuestions = questions.length
-
-	useEffect(() => {
-		setWrongAnswerMessage('')
-
-	}, [questionNumber])
 
 	if (numberOfQuestions === 0 || gameId === undefined || questionNumber === undefined) {
 		return (
@@ -58,7 +45,9 @@ export const Questions: React.FC = () => {
 		)
 	}
 
-	if (isNaN(parseInt(questionNumber)) || parseInt(questionNumber) < 0 || parseInt(questionNumber) > numberOfQuestions - 1) {
+	const questionIdx = parseInt(questionNumber)
+
+	if (isNaN(questionIdx) || questionIdx < 0 || questionIdx > numberOfQuestions - 1) {
 		return (
 			<Navigate
 				to={routeNames.game(gameId, true)}
@@ -66,7 +55,7 @@ export const Questions: React.FC = () => {
 			/>
 		)
 	}
-	const questionIdx = parseInt(questionNumber)
+
 	const onLastQuestion = questionIdx === numberOfQuestions - 1
 	const onFirstQuestion = questionIdx === 0
 
@@ -99,26 +88,6 @@ export const Questions: React.FC = () => {
 
 	const question = questions[questionIdx]
 
-	const onFormSubmit = async ({ answer }: FormValues) => {
-		try {
-			if (!team?.id) return
-			const { data } = await answerQuestion({
-				variables: {
-					answer: answer.trim(),
-					questionId: question.questionId,
-					teamId: team.id,
-				},
-			})
-			if (data?.answerQuestion.isCorrect === false) {
-				setWrongAnswerMessage(getWrongAnswerPhrases())
-			} else {
-				setWrongAnswerMessage('')
-			}
-		} catch (e) {
-			console.warn(e)
-		}
-	}
-
 	return (
 		<Flexbox
 			center
@@ -148,92 +117,11 @@ export const Questions: React.FC = () => {
 					</Button>
 				)}
 			</NavigationButtonContainer>
-			<QuestionHeader
-				center
-			>
-				<Row center>
-					<Text
-						size={18}
-						color={getRandomColor()}
-					>
-						Question
-					</Text>
-					&nbsp;
-					<Text
-						size={18}
-						color={getRandomColor()}
-					>
-						{questionIdx + 1}
-					</Text>
-
-				</Row>
-			</QuestionHeader>
-			<ContentContainer
-				center
-				full
-				marginBetween='medium'
-			>
-				<Flexbox
-					align='center'
-					marginBetween='small'
-				>
-					<Text
-						fontWeight='bold'
-						color={theme.color.yellow}
-						size={32}
-					>
-						{question.title}
-					</Text>
-					<StringText size={14}>{question.description}</StringText>
-				</Flexbox>
-				{question.image?.url && <QuestionImage src={question.image.url} />}
-			</ContentContainer>
-			<FormContainer
-				full
-				center
-				maxWidth={400}
-				marginBetween='medium'
-			>
-				{!question.isCorrect && (
-					<Form
-						initialValues={InitialFormValues}
-						validationSchema={formSchema}
-						validateOnBlur
-						onSubmit={onFormSubmit}
-					>
-						<Flexbox
-							marginBetween='medium'
-							paddingBottom='medium'
-						>
-							<TextInput
-								label='Answer'
-								name='answer'
-								placeholder='Submit your answer'
-							/>
-						</Flexbox>
-						<FormButton
-							primary
-							disabled={answerQuestionLoading}
-						>
-							Submit
-						</FormButton>
-					</Form>
-				)}
-				{question.isCorrect && (
-					<CorrectAnswerBanner
-						center
-					>
-						Correct!
-					</CorrectAnswerBanner>
-				)}
-				{wrongAnswerMessage && (
-					<WrongAnswerBanner
-						center
-					>
-						{wrongAnswerMessage}
-					</WrongAnswerBanner>
-				)}
-			</FormContainer>
+			<Question
+				question={question}
+				questionNumber={questionIdx}
+				teamId={team?.id}
+			/>
 			<Flexbox
 				marginBetween='small'
 				center
@@ -268,35 +156,5 @@ export const Questions: React.FC = () => {
 	)
 }
 
-const QuestionImage = styled.img`
-	object-fit: cover;
-	object-position: center;
-	max-width: 100%;
-	max-height: 350px;
-	width: auto;
-	height: auto;
-`
-
-const QuestionHeader = styled(Row)`
-`
-
-const ContentContainer = styled(Flexbox)`
-`
-
 const NavigationButtonContainer = styled(Row)`
-`
-
-const FormContainer = styled(Flexbox)`
-`
-
-const CorrectAnswerBanner = styled(Flexbox)`
-	border-radius: ${theme.borderRadius};
-	background-color: ${theme.color.darkGreen};
-	color: ${theme.color.white};
-	height: 44px;
-	width: 100%;
-`
-
-const WrongAnswerBanner = styled(CorrectAnswerBanner)`
-	background-color: ${theme.color.red};
 `
